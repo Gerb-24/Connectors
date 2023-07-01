8683cdfb4e766318c8a2ba9746e93adc19ddc772
Gerb-24 <gerbenlamers@gmail.com>
2023-07-01 23:14:51 +0200

    matrix rework
    changed some lists into matrices to be able to edit the neighbours, also changed to_walls to make use of normal_to_axes

--- second_attempt.py
+++ second_attempt.py
@@ -2,6 +2,24 @@ from src.PyVMF import *
 from main import *
 from sidewalls import prototypeVMF, BrushVertexManipulationBox, Textures
 
+class AbstractBrush:
+    def __init__( self, move_dic, mat_dic ):
+        self.move_dic = move_dic
+        self.mat_dic = mat_dic
+
+    def create( self, coord, textures, combined = None ):
+        proto = createDuplicateVMF(prototypeVMF)
+        verts = BrushVertexManipulationBox( proto ).createVerticesInBoxDict().moveToZero()
+        verts.translate( *coord )
+        verts.move_to_dic( self.move_dic )
+        textures.mat_to_dic( proto, self.mat_dic )
+
+        if combined:
+            for vertex in verts.verticesDict[ combined['keys'][ 0 ] ]:
+                if vertex in verts.verticesDict[ combined['keys'][ 1 ] ]:
+                    vertex.move( *verts.getMove( 'z', combined['val'] ))
+
+        return proto
 
 class FaceData:
     def __init__(self, normal, pos,  dims ):
@@ -23,42 +41,30 @@ class FaceData:
         if x1_min >= self.x1_max or x2_min >= self.x2_max or x1_max <= self.x1_min or x2_max <= self.x2_min:
             return 0
         else:
-            ans = 1 if self.normal in {'x', 'y', 'z'} else -1
-            return ans
+            return 1
+
+    def has_zero_volume( self ):
+        return (self.x1_min >= self.x1_max or self.x2_min >= self.x2_max)
 
     def to_wall( self, textures ):
-        proto = createDuplicateVMF(prototypeVMF)
-        verts = BrushVertexManipulationBox( proto ).createVerticesInBoxDict().moveToZero()
+        if self.has_zero_volume():
+            proto = new_vmf()
+            return proto
 
-        if self.normal == 'x':
-            verts.full_move( self.pos, self.x1_max, self.x2_max, self.pos - 128, self.x1_min, self.x2_min  )
-
-        elif self.normal == 'y':
-            verts.full_move( self.x1_max, self.pos, self.x2_max, self.x1_min, self.pos - 128, self.x2_min  )
-
-        elif self.normal == '-x':
-            verts.full_move( self.pos + 128, self.x1_max, self.x2_max, self.pos, self.x1_min, self.x2_min  )
-
-        elif self.normal == '-y':
-            verts.full_move( self.x1_max, self.pos + 128, self.x2_max, self.x1_min, self.pos, self.x2_min  )
-
-        elif self.normal == 'z':
-            verts.full_move( self.x1_max, self.x2_max, self.pos, self.x1_min, self.x2_min, self.pos - 128  )
-
-        elif self.normal == '-z':
-            verts.full_move( self.x1_max, self.x2_max, self.pos + 128, self.x1_min, self.x2_min, self.pos  )
-
-        solid = proto.get_solids()[0]
-        for side in solid.get_sides():
-            if side.material in textures.to_sides( self.normal ):
-                if self.normal == 'z':
-                    side.material = 'orange_dev/dev_measurewall_green03'.upper()
-                elif self.normal == '-z':
-                    side.material = 'dev/reflectivity_70'.upper()
-                else:
-                    side.material = 'dev/dev_blendmeasure2'.upper()
-            else:
-                side.material = textures.nodraw
+        materials_dictionary = {
+                self.normal:                            NORMAL_TO_TEXTURE(self.normal),
+            }
+        x1, x2 = NORMAL_TO_AXES( self.normal )
+        sign = DIRECTION_IS_POSITIVE( self.normal )
+        move_dictionary = {
+            self.normal:                                -1*sign*self.pos,
+            DIRECTION_HANDLER(self.normal, 'o'):        sign*(self.pos + sign*128),
+            x1:                                         self.x1_max,
+            DIRECTION_HANDLER(x1, 'o'):                 -1*self.x1_min,
+            x2:                                         self.x2_max,
+            DIRECTION_HANDLER(x2, 'o'):                 -1*self.x2_min,
+        }
+        proto = AbstractBrush(move_dictionary, materials_dictionary).create( (0, 0, 0), textures )
         return proto
 
 def DICT_LIST_HELPER( dict, key, obj ):
@@ -100,9 +106,21 @@ def DIRECTION_HANDLER( direction, s ):
                     '-':    '-y',
                     'o':    'y',
                     },
+                'z':
+                    {
+                    'o':    '-z',
+                    },
+                '-z':
+                    {
+                    'o':    'z',
+                    }
                 }
     return direction_dict[direction][s]
 
+def DIRECTION_IS_POSITIVE( direction ):
+    if direction in { 'x', 'y', 'z' }:
+        return -1
+    return 1
 
 def NORMAL_TO_AXES( normal ):
     ''' I am not sure if I will ever need this, but it seems good to keep track of '''
@@ -115,6 +133,14 @@ def NORMAL_TO_AXES( normal ):
         '-z':   ('x', 'y'),
     }
     return dict[normal]
+
+def NORMAL_TO_TEXTURE( normal ):
+    if normal == 'z':
+        return 'orange_dev/dev_measurewall_green03'.upper()
+    elif normal == '-z':
+        return 'dev/reflectivity_70'.upper()
+    else:
+        return 'dev/dev_blendmeasure2'.upper()
 
 def GET_FACE_DATA( solid ):
     xMin, xMax, yMin, yMax, zMin, zMax = getDimensionsOfSolid( solid )
@@ -151,7 +177,7 @@ for fd in face_data_list:
 
 
 
-def get_new_faces( fd_direction_dict, pos_neg_dir ):
+def get_new_faces( fd_direction_dict ):
     all_new_faces = []
     for pos in fd_direction_dict:
         x1_set, x2_set = set([]), set([])
@@ -162,29 +188,52 @@ def get_new_faces( fd_direction_dict, pos_neg_dir ):
         x2_list = list(x2_set)
         x1_list.sort()
         x2_list.sort()
-        pre_face_list = []
-        for i in range( len(x1_list) - 1):
-            for j in range( len(x2_list) - 1 ):
-                pre_face_list.append( (x1_list[i], x1_list[i+1], x2_list[j], x2_list[j+1]) )
-        intersection_numbers = [0]*len(pre_face_list)
-                
-        for i in range( len(pre_face_list) ):
-            for fd in fd_direction_dict[pos]:
-                intersection_numbers[i] += fd.has_intersection(pre_face_list[i])
+        pre_face_matrix = []
+        intersection_matrix = []
+        normal_matrix = []
+        for i in range(len(x1_list)-1):
+            pre_face_matrix.append([])
+            intersection_matrix.append([])
+            normal_matrix.append([])
+            for j in range(len(x2_list)-1):
+                normal_matrix[i].append('?')
+                intersection_matrix[i].append(0)
+                pre_face_matrix[i].append( [x1_list[i], x1_list[i+1], x2_list[j], x2_list[j+1]] )
+                for fd in fd_direction_dict[pos]:
+                    has_intersection =  fd.has_intersection(pre_face_matrix[i][j])
+                    intersection_matrix[i][j] += has_intersection
+                    if has_intersection in {1, -1}:
+                        normal_matrix[i][j] = fd.normal
+        
+
+        for i in range(len(x1_list)-1):
+            for j in range(len(x2_list)-1):
+                if intersection_matrix[i][j] == 2:
+                    pre_face_matrix[i+1][j][0] += 128
+                    pre_face_matrix[i-1][j][1] -= 128
+                    pre_face_matrix[i][j+1][2] += 128
+                    pre_face_matrix[i][j-1][3] -= 128
+
+
         new_faces = []
-        for i in range( len(pre_face_list) ):
-            if intersection_numbers[i]!= 0:
-                normal = pos_neg_dir[0] if intersection_numbers[i] == 1 else pos_neg_dir[1]
-                new_faces.append( FaceData(normal, pos, pre_face_list[i]) )
+        for i in range(len(x1_list)-1):
+            for j in range(len(x2_list)-1):
+                if intersection_matrix[i][j] == 1:
+                    new_faces.append( FaceData(normal_matrix[i][j], pos, pre_face_matrix[i][j]) )
 
         all_new_faces.extend( new_faces )
     return all_new_faces
 
 all_new_faces = []
-all_new_faces.extend( get_new_faces( fd_x_dict, ['x', '-x'] ) )
-all_new_faces.extend( get_new_faces( fd_y_dict, ['y', '-y'] ) )
-all_new_faces.extend( get_new_faces( fd_z_dict, ['z', '-z'] ) )
+all_new_faces.extend( get_new_faces( fd_x_dict ) )
+print('done for x')
+all_new_faces.extend( get_new_faces( fd_y_dict ) )
+print('done for y')
+all_new_faces.extend( get_new_faces( fd_z_dict ) )
+print('done for z')
 
 for fd in all_new_faces:
     dummy_vmf = addVMF(dummy_vmf, fd.to_wall(textures))
 dummy_vmf.export('vmfs/test3_new_connected.vmf')
+
+print('done')
